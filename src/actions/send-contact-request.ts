5ffca97d139f0c369eee3b024b5ae5a9c94b8a05
Jordan <jtaccinelli@gmail.com>
2025-06-14 14:03:29 +1000

    fix: ammended border, added error handling for action

---
+++ src/actions/send-contact-request.ts
@@ -0,0 +1,36 @@
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
+import { EmailMessage } from "cloudflare:email";
+import { createMimeMessage } from "mimetext";
+
+export const sendContactRequest = defineAction({
+  accept: "form",
+  input: z.object({
+    email: z.string(),
+    name: z.string(),
+    message: z.string(),
+  }),
+  async handler(input, context) {
+    const { EMAIL } = context.locals.runtime.env;
+
+    const body = `
+        Name: ${input.name}
+        Email: ${input.email}
+        Message: ${input.message}
+      `;
+
+    const sender = "contact@email.jtaccinelli.com.au";
+    const recipient = "contact@email.jtaccinelli.com.au";
+    const email = createMimeMessage();
+
+    email.setRecipient(recipient);
+    email.setSender({ name: "Porfolio Contact Form", addr: sender });
+    email.setSubject("New contact form submission");
+    email.addMessage({ contentType: "text/plain", data: body });
+
+    const message = new EmailMessage(sender, recipient, email.asRaw());
+    await EMAIL.send(message);
+
+    return new Response("Tester");
+  },
+});
